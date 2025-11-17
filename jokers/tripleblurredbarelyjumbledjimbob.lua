9c6618381109ea0730ff416c7171df7530bd1656
KylomaskGamer <kylomask@kylomaskgamer.ca>
2025-11-16 20:27:16 -0400

    Update position and attributes in tripleblurredbarelyjumbledjimbob

--- jokers/tripleblurredbarelyjumbledjimbob.lua
+++ jokers/tripleblurredbarelyjumbledjimbob.lua
@@ -16,8 +16,8 @@ SMODS.Joker{ --Triple-Blurred Barely Jumbled Jimbob
         }
     },
     pos = {
-        x = 5,
-        y = 2
+        x = 0,
+        y = 0
     },
     display_size = {
         w = 71 * 1, 
@@ -30,6 +30,46 @@ SMODS.Joker{ --Triple-Blurred Barely Jumbled Jimbob
     perishable_compat = true,
     unlocked = true,
     discovered = true,
-    atlas = 'CustomJokers',
-    pools = { ["kylomask_kylomask_jokers"] = true }
-}
+    atlas = 'Joker',
+    pools = { ["modprefix_kylomask_jokers"] = true },
+
+    
+    calculate = function(self, card, context)
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        -- Combine suits effect enabled
+        -- Combine suits effect enabled
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        -- Combine suits effect disabled
+        -- Combine suits effect disabled
+    end
+} 
+      
+local card_is_suit_ref = Card.is_suit
+function Card:is_suit(suit, bypass_debuff, flush_calc)
+    local ret = card_is_suit_ref(self, suit, bypass_debuff, flush_calc)
+    if not ret and not SMODS.has_no_suit(self) then
+        if next(SMODS.find_card("j_modprefix_tripleblurredbarelyjumbledjimbob")) then
+            -- If checking for Spades and card is Hearts, return true
+            if suit == "Spades" and self.base.suit == "Hearts" then
+                ret = true
+            end
+            -- If checking for Hearts and card is Spades, return true
+            if suit == "Hearts" and self.base.suit == "Spades" then
+                ret = true
+            end
+            -- If checking for Diamonds and card is Clubs, return true
+            if suit == "Diamonds" and self.base.suit == "Clubs" then
+                ret = true
+            end
+            -- If checking for Clubs and card is Diamonds, return true
+            if suit == "Clubs" and self.base.suit == "Diamonds" then
+                ret = true
+            end
+        end
+    end
+    return ret
+end
