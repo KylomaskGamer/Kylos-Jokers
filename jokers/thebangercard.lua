d967e1352e8e17906435b217c41c311f0381cb7c
KylomaskGamer <kylomask@kylomaskgamer.ca>
2025-11-17 09:43:06 -0400

    Remove custom logic for thebangercard
    Removed custom buy space and selection logic for thebangercard.

--- jokers/thebangercard.lua
+++ jokers/thebangercard.lua
@@ -46,21 +46,3 @@ SMODS.Joker{ --The Banger Card
         end
     end
 }
-
-local check_for_buy_space_ref = G.FUNCS.check_for_buy_space
-G.FUNCS.check_for_buy_space = function(card)
-    if card.config.center.key == "j_kylomask_thebangercard" then -- ignore slot limit when bought
-        return true
-    end
-    return check_for_buy_space_ref(card)
-end
-
-local can_select_card_ref = G.FUNCS.can_select_card
-G.FUNCS.can_select_card = function(e)
-	if e.config.ref_table.config.center.key == "j_kylomask_thebangercard" then
-		e.config.colour = G.C.GREEN
-		e.config.button = "use_card"
-	else
-		can_select_card_ref(e)
-	end
-end
